bdffb7e2978ecc7ceb5d512b5072d866490bf65e
Taylor Donlan <taylordonlan@gmail.com>
2018-04-18 21:41:17 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -48,6 +48,7 @@ class MusicLibraryController
  end
 
  def list_songs
+   Song.all.each_with_index {|song,index|puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
 
  end
 
