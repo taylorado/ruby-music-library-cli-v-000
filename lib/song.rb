9e1d301e11c007753d78a7cf6cec2d58b70726fc
Taylor Donlan <taylordonlan@gmail.com>
2018-04-18 20:59:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,16 @@ class Song
     genre.add_song(self)
   end
 
+  def self.new_from_filename(filename)
+    title = filename.split(" - ")
+    artist = Artist.find_or_create_by_name(title[0])
+    genre = Genre.find_or_create_by_name(title[2][0..-5])
+    create(title[1], artist, genre)
+  end 
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename)
+  end
 
 
 end
