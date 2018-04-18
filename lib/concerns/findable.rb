0ef74d5cf475586f3149ee94f4abe1d145403f25
Taylor Donlan <taylordonlan@gmail.com>
2018-04-18 20:14:24 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,12 +4,12 @@ module Concerns::Findable
       self.all.detect { |x| x.name == name }
     end
 
-    def find_or_create_by_name(name)
-      if find_by_name(name).nil?
-        self.create(name)
-      else
-        find_by_name(name)
-      end
-    end
+  #  def find_or_create_by_name(name)
+  #    if find_by_name(name).nil?
+  #      self.create(name)
+  #    else
+  #      find_by_name(name)
+  #    end
+  #  end
   end
 end
