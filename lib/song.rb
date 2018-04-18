7eda97ef999d20ab0dcc513eb9cc15cb33d9952f
Taylor Donlan <taylordonlan@gmail.com>
2018-04-18 19:49:11 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all =[]
+
+  def initialize(name, artist=nil, genre=nil)
+    self.name = name
+    self.artist = artist unless artist=nil
+    self.genre = genre unless genre=nil
+  end
+  
